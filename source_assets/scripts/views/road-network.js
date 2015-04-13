cd81736fa1c446aef1666468430a0f76052cb22a
Marc Farra <marcfarra@gmail.com>
2015-04-13 15:12:17 -0400

    Add datatables

--- source_assets/scripts/views/road-network.js
+++ source_assets/scripts/views/road-network.js
@@ -1,7 +1,15 @@
 'use strict';
 
 var BaseView = require('./base-view.js');
+var dataTable = require('datatables');
+var $ = require('jquery');
+$.DataTable = dataTable;
 
 module.exports = BaseView.extend({
   template: require('../templates/road-network.html'),
+  render: function () {
+    BaseView.prototype.render.call(this);
+    $('.table').dataTable();
+    return this;
+  }
 });
