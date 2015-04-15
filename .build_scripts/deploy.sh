a8fb63974668be760b2e8559b10566240c043814
Olaf Veerman <olaf.veerman@flipside.org>
2015-04-15 11:49:51 +0100

    Have Travis build and deploy site to gh-pages. Fix #30
    Add correct repo

---
+++ .build_scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+# If this is the deploy branch, push it up to gh-pages
+if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
+  echo "Get ready, we're pushing to gh-pages!"
+  git init
+  git config user.name "Travis-CI"
+  git config user.email "travis@somewhere.com"
+  git add index.html
+  git add assets
+  git commit -m "CI deploy to gh-pages"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+  rm -rf .git/
+else
+  echo "Not a publishable branch so we're all done here"
+fi
