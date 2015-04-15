567a201899f47d8ab3759f633cb7640aadfc5bb5
Olaf Veerman <olaf.veerman@flipside.org>
2015-04-15 16:43:18 +0100

    Don't make Travis serve site locally. Use gulp build

--- .build_scripts/build.sh
+++ .build_scripts/build.sh
@@ -4,7 +4,7 @@ set -e # halt script on error
 # If this is the deploy branch, build it
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Building site"
-  gulp
+  gulp build
 else
   echo "Not deploying, so long and thanks for all the fish!"
 fi
