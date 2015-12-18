e62e95b38be6e12ae312291c336745a8a6ded1e3
Daniel Silva <daniel@developmentseed.org>
2015-12-18 18:04:40 -0500

    Comments

--- app/assets/scripts/views/editor.js
+++ app/assets/scripts/views/editor.js
@@ -10,6 +10,30 @@ var Editor = React.createClass({
     dispatch: React.PropTypes.func
   },
 
+  // /////////////////////////////////////////////////////////////////////////////
+  // / Message listener (postMessage)
+  // /
+  // / When receiving a message form the iframe, process the url and set it.
+  // / The url now becomes shareable. The action to take on the url will depend on
+  // / the app.
+  // /
+  // / The switch is done based on the app id, defined when the OR_frame_notifier
+  // / is included.
+  // /
+  // /
+  // / What this actually does:
+  // / When a the iframe's url changes it is sent via postMessage to the parent. It
+  // / removes the base portion on the url (defined in the config) and stores
+  // / the rest:
+  // / - Url on the "editor" changes to:
+  // /     http://devseed.com/editor/#background=something
+  // / - Prefix is removed resulting in:
+  // /     #background=something
+  // / - Appended to the current url:
+  // /     http://devseed.com/openroads/#/editor/background=something
+  // / - When entering the page, everything after (/#/editor/)
+  // /   is sent to the iframe alongside the proper prefix.
+  // /
   messageListener: function (e) {
     if (e.data.type === 'urlchange') {
       switch (e.data.id) {
