25633d3aa70268a6f98b2501d92f573167ced379
Daniel da Silva <daniel@developmentseed.org>
2016-03-08 14:20:58 +0000

    Setup chart structure. Remove uneeded module

--- app/assets/scripts/components/charts/pie-chart.js
+++ app/assets/scripts/components/charts/pie-chart.js
@@ -1,47 +1,118 @@
 'use strict';
 import React from 'react';
-import hat from 'hat';
 import _ from 'lodash';
+import d3 from 'd3';
 
 var PieChart = React.createClass({
   displayName: 'PieChart',
 
   propTypes: {
+    className: React.PropTypes.string
   },
 
   chart: null,
-  chartId: null,
 
   onWindowResize: function () {
     this.chart.update();
   },
 
-  componentWillMount: function () {
-    this.chartId = 'piechart-' + hat();
-  },
-
   componentDidMount: function () {
+    // console.log('PieChart componentDidMount');
+    // Debounce event.
     this.onWindowResize = _.debounce(this.onWindowResize, 200);
+
     window.addEventListener('resize', this.onWindowResize);
-    this.chart = new Pie(this.chartId, this.props);
+    this.chart = new Chart(this.refs.container, this.props);
   },
 
   componentWillUnmount: function () {
+    // console.log('PieChart componentWillUnmount');
     window.removeEventListener('resize', this.onWindowResize);
-    // this.chart.destroy();
+    this.chart.destroy();
   },
 
-  componentDidUpdate: function () {
-    // this.chart.setData(this.props);
+  componentDidUpdate: function (/* prevProps, prevState */) {
+    // console.log('PieChart componentDidUpdate');
+    this.chart.setData(this.props);
   },
 
   render: function () {
-    return <div className="pie-chart" id={this.chartId}></div>;
+    return (
+      <div className={this.props.className} ref='container'></div>
+    );
   }
 });
 
-var Pie = function (id, props) {
-
-};
-
 module.exports = PieChart;
+
+var Chart = function (el, data) {
+  this.$el = d3.select(el);
+
+  this.data = null;
+  this.stages = null;
+
+  var _this = this;
+
+  // Var declaration.
+  var margin = {top: 16, right: 16, bottom: 48, left: 48};
+  // width and height refer to the data canvas. To know the svg size the margins
+  // must be added.
+  var _width, _height;
+  // Elements.
+  var svg, dataCanvas;
+
+  this._calcSize = function () {
+    _width = parseInt(this.$el.style('width'), 10) - margin.left - margin.right;
+    _height = parseInt(this.$el.style('height'), 10) - margin.top - margin.bottom;
+  };
+
+  this.setData = function (data) {
+    var _data = _.cloneDeep(data);
+    // TODO: Set data depends on the prop name used
+    // this.data = _data.data
+    this.update();
+  };
+
+  this._init = function () {
+    this._calcSize();
+    // The svg.
+    svg = this.$el.append('svg')
+        .attr('class', 'chart');
+
+    // Chart elements
+    dataCanvas = svg.append('g')
+      .attr('class', 'data-canvas')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    // TODO:
+    // Init chart elements.
+  };
+
+  this.update = function () {
+    if (this.data === null) {
+      return;
+    }
+    this._calcSize();
+
+    svg
+      .attr('width', _width + margin.left + margin.right)
+      .attr('height', _height + margin.top + margin.bottom);
+
+    dataCanvas
+      .attr('width', _width)
+      .attr('height', _height);
+
+    // TODO:
+    // Update chart elements.
+  };
+
+  this.destroy = function () {
+    // TODO:
+    // Elements to remove.
+  };
+
+  // ------------------------------------------------------------------------ //
+  // 3... 2... 1... GO...
+  this._init();
+  this.setData(data);
+};
