2c4c27fae24f97e00737f1b1be9f8aeb84ad8c7b
Daniel da Silva <daniel@developmentseed.org>
2016-06-07 09:41:24 +0100

    Add missing class

--- app/assets/scripts/views/projects.js
+++ app/assets/scripts/views/projects.js
@@ -113,7 +113,7 @@ var Projects = React.createClass({
     return (
       <ReactPaginate previousLabel={<span>previous</span>}
         nextLabel={<span>next</span>}
-        breakLabel={<span>...</span>}
+        breakLabel={<span className='pages__page'>...</span>}
         pageNum={this.getTotalPages()}
         forceSelected={this.getPage() - 1}
         marginPagesDisplayed={2}
