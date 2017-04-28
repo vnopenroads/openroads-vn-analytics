a5bb0b2bdb38ee824986651258cde734b0ae85f4
Drew Bollinger <drew@developmentseed.org>
2017-04-28 17:52:58 -0400

    Add production editor url

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -8,7 +8,6 @@ module.exports = {
   consoleMessage: logo,
   api: 'http://openroads-api.herokuapp.com',
   mbToken: 'pk.eyJ1Ijoib3BlbnJvYWRzIiwiYSI6InJ0aUQ2N3MifQ.R3hdFqriZr6kEUr-j_FYpg',
-  editorUrl: '//opengovt.github.io/openroads-iD/',
+  editorUrl: '//orma.github.io/openroads-vn-iD/',
   roadNetTileLayerUrl: 'http://50.16.162.86/dashboard/{z}/{x}/{y}.png'
 };
-
