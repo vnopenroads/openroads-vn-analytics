82f585f7f623dfc780e978eddc495c7a7cc78492
Nick Bumbarger <nbumbarger@gmail.com>
2017-05-03 17:47:11 -0400

    Add completion checking for the multi-province data structure

--- app/assets/scripts/views/analytics-admin-area.js
+++ app/assets/scripts/views/analytics-admin-area.js
@@ -1,6 +1,7 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { fetchVProMMSids } from '../actions/action-creators';
 
 var AnalyticsAA = React.createClass({
@@ -18,19 +19,33 @@ var AnalyticsAA = React.createClass({
   },
 
   render: function () {
-    const ids = this.props.VProMMSids.data;
-    const done = ids.filter(v => v.inTheDatabase).length;
-    const total = ids.length;
-    const percentageComplete = (done / total).toFixed(2);
+    const provinceData = this.props.VProMMSids.data;
+    let accumulator = { done: 0, total: 0 };
+    const ids = _.map(provinceData, (data, id) => {
+      const name = data.provinceName;
+      const done = data.vpromms.filter(v => v.inTheDatabase).length;
+      const total = data.vpromms.length;
+      accumulator.done += done;
+      accumulator.total += total;
+      const percentageComplete = (done / total).toFixed(2);
+      return {
+        id,
+        name,
+        done,
+        total,
+        percentageComplete
+      };
+    });
+    const { done, total } = accumulator;
     return (
       <section className='page'>
         <div className='page__body aa'>
           <div className='aa-main'>
-            <h2 className='complete'>{percentageComplete} % of VProMMS Ids added ({done} of {total})</h2>
+            <h2 className='complete'>{(accumulator.done / accumulator.total).toFixed(2)} % of VProMMS Ids added ({done.toLocaleString()} of {total.toLocaleString()})</h2>
             <ul>
-              {ids.map(v => {
+              {/* {ids.map(v => {
                 return <li key={v.id}>{v.id}: {v.inTheDatabase ? 'added' : 'not added'}</li>;
-              })}
+              })} */}
             </ul>
           </div>
         </div>
