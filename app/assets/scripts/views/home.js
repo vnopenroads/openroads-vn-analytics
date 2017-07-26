1733a1b364861a1b03a7a104ac15a8244163c884
Ricardo Mestre <ricardo@duplos.org>
2017-07-26 15:25:36 +0100

    Include country label
    Also, fix navigation stacking on small screens.

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -11,7 +11,7 @@ var Home = React.createClass({
         <header className='page__header--landing'>
           <div className='page__headline--landing'>
             <h1 className='page__title--landing'><img src='assets/graphics/layout/openroads-vn-logo-hor-neg.svg' width='736' height='96' alt='OpenRoads Vietnam logo' /><span>OpenRoads</span> <strong>Vietnam</strong></h1>
-            <p className='page__description--landing'>Mapping, tracking and visualizing road projects in Vietnam for inclusive growth</p>
+            <p className='page__description--landing'>Mapping, tracking and visualizing road projects in Vietnam for inclusive growth.</p>
           </div>
         </header>
 
@@ -20,7 +20,7 @@ var Home = React.createClass({
             <h2>Access and improve Road Networks</h2>
             <p className='description'></p>
             <p className='description'>Work with the OpenRoads project to close this critical information gap and create a comprehensive road network map of Vietnam.</p>
-            <Link to='/analytics' className='bttn-explore'>Explore more</Link>
+            <Link to='/analytics' className='bttn-explore'>Explore</Link>
           </div>
         </div>
       </section>
