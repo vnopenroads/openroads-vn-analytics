70c315c7f9f4aa19b1e649832989f31c18f08fed
Olaf Veerman <olaf.veerman@gmail.com>
2017-10-11 16:05:54 -0400

    Update tile and editor url

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -8,7 +8,7 @@ module.exports = {
   consoleMessage: logo,
   api: 'http://api.openroads-vn.com',
   mbToken: 'pk.eyJ1Ijoib3BlbnJvYWRzIiwiYSI6InJ0aUQ2N3MifQ.R3hdFqriZr6kEUr-j_FYpg',
-  editorUrl: '//id.openroads-vn.com/',
-  roadNetTileLayerUrl: 'http://54.255.146.89:3000/dashboard/{z}/{x}/{y}.png',
+  editorUrl: '//editor.openroads-vn.com/',
+  roadNetTileLayerUrl: 'http://api.openroads-vn.com:3000/dashboard/{z}/{x}/{y}.png',
   provinceDumpBaseUrl: 'https://s3.amazonaws.com/openroads-vn-dumps/by-province-id/'
 };
