551fb43e4901ee321216380b7c6485334f3e6006
Miles Watkins <miles.w.watkins@gmail.com>
2017-10-15 17:03:04 +0700

    Hotfix to RLP upload issue, by including a name to the multipart upload file

--- app/assets/scripts/views/upload.js
+++ app/assets/scripts/views/upload.js
@@ -10,14 +10,14 @@ var Upload = React.createClass({
           <div className='upload__header'>Upload RoadLabPro ZIP files</div>
           <form method="post" encType="multipart/form-data" action={config.api + '/fielddata/properties/rlp'}>
             <div>
-              <input type="file" id="rlp-zip-properties" accept=".zip" />
+              <input type="file" id="rlp-zip-properties" name="upload-file-needs-to-have-a-name" accept=".zip" />
               <button>Ingest road properties</button>
             </div>
           </form>
 
           <form method="post" encType="multipart/form-data" action={config.api + '/fielddata/geometries/rlp'}>
             <div>
-              <input type="file" id="rlp-zip-geometries" accept=".zip" />
+              <input type="file" id="rlp-zip-geometries" name="upload-file-needs-to-have-a-name" accept=".zip" />
               <button>Ingest road geometries</button>
             </div>
           </form>
