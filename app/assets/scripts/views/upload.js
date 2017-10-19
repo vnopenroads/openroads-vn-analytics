2ab3c311e846d2cc6cfbfa200cef7516d7c26304
Ricardo Mestre <ricardo@duplos.org>
2017-10-19 17:53:06 +0100

    Structure upload page

--- app/assets/scripts/views/upload.js
+++ app/assets/scripts/views/upload.js
@@ -5,22 +5,31 @@ import config from '../config';
 var Upload = React.createClass({
   render: function () {
     return (
-      <section classNmae='page'>
-        <div className='page__body'>
-          <div className='upload__header'>Upload RoadLabPro ZIP files</div>
-          <form method="post" encType="multipart/form-data" action={config.api + '/fielddata/properties/rlp'}>
-            <div>
-              <input type="file" id="rlp-zip-properties" name="upload-file-needs-to-have-a-name" accept=".zip" />
-              <button>Ingest road properties</button>
+      <section classNmae='inpage'>
+        <header className='inpage__header'>
+          <div className='inner'>
+            <div className='inpage__headline'>
+              <h1 className='inpage__title'>Upload</h1>
             </div>
-          </form>
+          </div>
+        </header>
+        <div className='inpage__body'>
+          <div className='inner'>
+            <div className='upload__header'>Upload RoadLabPro ZIP files</div>
+            <form method="post" encType="multipart/form-data" action={config.api + '/fielddata/properties/rlp'}>
+              <div>
+                <input type="file" id="rlp-zip-properties" name="upload-file-needs-to-have-a-name" accept=".zip" />
+                <button>Ingest road properties</button>
+              </div>
+            </form>
 
-          <form method="post" encType="multipart/form-data" action={config.api + '/fielddata/geometries/rlp'}>
-            <div>
-              <input type="file" id="rlp-zip-geometries" name="upload-file-needs-to-have-a-name" accept=".zip" />
-              <button>Ingest road geometries</button>
-            </div>
-          </form>
+            <form method="post" encType="multipart/form-data" action={config.api + '/fielddata/geometries/rlp'}>
+              <div>
+                <input type="file" id="rlp-zip-geometries" name="upload-file-needs-to-have-a-name" accept=".zip" />
+                <button>Ingest road geometries</button>
+              </div>
+            </form>
+          </div>
         </div>
       </section>
     );
