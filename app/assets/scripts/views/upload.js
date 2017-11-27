2bd5a4ff03d7ea976fdd75e11b16b6eb2cf7cfb7
James Conkling <james.lane.conkling@gmail.com>
2017-11-27 17:25:12 -0500

    update translations for upload

--- app/assets/scripts/views/upload.js
+++ app/assets/scripts/views/upload.js
@@ -16,23 +16,23 @@ var Upload = React.createClass({
         </header>
         <div className='inpage__body'>
           <div className='inner'>
-            <h2>Upload RoadLabPro ZIP files</h2>
+            <h2>{t('Upload RoadLabPro ZIP files')}</h2>
             <form className='form upload-form' method='post' encType='multipart/form-data' action={config.api + '/fielddata/properties/rlp'}>
               <div className='form__group'>
-                <label className='form__label' htmlFor='rlp-zip-properties'>Road properties</label>
+                <label className='form__label' htmlFor='rlp-zip-properties'>{t('Road properties')}</label>
                 <div className='form__input-group form__input-group--medium'>
                   <input type='file' className='form__control' id='rlp-zip-properties' name='rlp-zip-properties' accept='.zip' />
-                  <button type='button' className='button button--secondary-raised-dark' title='Submit'><span>Upload</span></button>
+                  <button type='button' className='button button--secondary-raised-dark' title='Submit'><span>{t('Upload')}</span></button>
                 </div>
               </div>
             </form>
 
             <form className='form upload-form' method='post' encType='multipart/form-data' action={config.api + '/fielddata/geometries/rlp'}>
               <div className='form__group'>
-                <label className='form__label' htmlFor='rlp-zip-geometries'>Road geometries</label>
+                <label className='form__label' htmlFor='rlp-zip-geometries'>{t('Road geometries')}</label>
                 <div className='form__input-group form__input-group--medium'>
                   <input type='file' className='form__control' id='rlp-zip-geometries' name='rlp-zip-geometries' accept='.zip' />
-                  <button type='button' className='button button--secondary-raised-dark' title='Submit'><span>Upload</span></button>
+                  <button type='button' className='button button--secondary-raised-dark' title='Submit'><span>{t('Upload')}</span></button>
                 </div>
               </div>
             </form>
