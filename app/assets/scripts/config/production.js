1709da4267125c6932d1bf15a054b2a74596c59d
James Conkling <james.lane.conkling@gmail.com>
2017-11-29 12:27:42 -0500

    don't check in dev changes to production config

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -6,8 +6,7 @@ var logo = require('./logo');
 module.exports = {
   environment: 'production',
   consoleMessage: logo,
-  // api: 'http://api.openroads-vn.com',
-  api: 'http://localhost:4000',
+  api: 'http://api.openroads-vn.com',
   mbToken: 'pk.eyJ1Ijoib3BlbnJvYWRzIiwiYSI6InJ0aUQ2N3MifQ.R3hdFqriZr6kEUr-j_FYpg',
   editorUrl: '//editor.openroads-vn.com/',
   roadNetTileLayerUrl: 'http://api.openroads-vn.com:3000/dashboard/{z}/{x}/{y}.png',
