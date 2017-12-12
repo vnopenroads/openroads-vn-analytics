d77179ed7d89ac524044b7b147a31585882b049b
James Conkling <james.lane.conkling@gmail.com>
2017-12-12 16:52:27 -0500

    simplify table markdown

--- app/assets/scripts/components/road-table.js
+++ app/assets/scripts/components/road-table.js
@@ -22,48 +22,47 @@ const RoadTable = ({
           roadCount={roadCount}
         />
     }
+
     <CreateRoadForm />
 
     {roadsPageStatus === 'pending' ?
       <div className='a-subnav loading'><h2><T>Loading</T></h2></div> :
-      <div>
-        <div className="table">
-          <table>
-            <thead>
-              <tr>
-                <th className="table-properties-head button-column" />
-                <ColumnHeader
-                  columnKey="id"
-                  label="VPRoMMS ID"
-                  sortField="id"
-                  sortOrder={sortOrder}
-                  sortColumnAction={sortColumnAction}
-                />
-                <th className='table-properties-head'><T>Field Data</T></th>
-                <th className='table-properties-head'><T>Properties</T></th>
-              </tr>
-            </thead>
-            <tbody>
-              {_.map(roadsPage, (vpromm) => (
-                <Row
-                  key={vpromm}
-                  vpromm={vpromm}
-                  adminRoadProperties={adminRoadProperties}
-                  vprommFieldInDB={fieldRoads.includes(vpromm)}
-                />
-              ))}
-            </tbody>
-          </table>
-        </div>
-        {
-          roadCountStatus === 'complete' &&
-            <RoadPagination
-              page={page}
-              roadPageCount={roadPageCount}
-              setPage={setPage}
-            />
-        }
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th className="table-properties-head button-column" />
+              <ColumnHeader
+                columnKey="id"
+                label="VPRoMMS ID"
+                sortField="id"
+                sortOrder={sortOrder}
+                sortColumnAction={sortColumnAction}
+              />
+              <th className='table-properties-head'><T>Field Data</T></th>
+              <th className='table-properties-head'><T>Properties</T></th>
+            </tr>
+          </thead>
+          <tbody>
+            {_.map(roadsPage, (vpromm) => (
+              <Row
+                key={vpromm}
+                vpromm={vpromm}
+                adminRoadProperties={adminRoadProperties}
+                vprommFieldInDB={fieldRoads.includes(vpromm)}
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
+    }
+    {
+      roadCountStatus === 'complete' &&
+        <RoadPagination
+          page={page}
+          roadPageCount={roadPageCount}
+          setPage={setPage}
+        />
     }
   </div>
 );
