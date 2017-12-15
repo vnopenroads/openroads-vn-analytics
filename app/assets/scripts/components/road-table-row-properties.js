f7bd96a175394e0f4a78bf071525cb7ae9785442
Miles Watkins <miles.w.watkins@gmail.com>
2017-12-15 17:16:40 -0500

    Add chart to compare road lengths, closes #274

--- app/assets/scripts/components/road-table-row-properties.js
+++ app/assets/scripts/components/road-table-row-properties.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-  map
+  map,
+  round
 } from 'lodash';
 
 
@@ -9,19 +10,48 @@ const RowProperties = ({ properties }) => (
     className="table-properties"
   >
     <dl className='table-properties-list'>
+      <LengthChart
+        platformLength={properties['length']}
+        tabularLength={properties['Road Length (VProMMS)']}
+      />
       {
-        map(properties, (prop, key) => (
-          <div
-            key={key}
-          >
-            <dt>{key}</dt>
-            <dd>{prop}</dd>
-          </div>
-        ))
+        map(properties, (prop, key) =>
+          // Since we have the above chart, no need to include length values
+          key.startsWith('Road Length') || key === 'length'
+            ? ''
+            : (
+              <div
+                key={key}
+              >
+                <dt>{key}</dt>
+                <dd>{prop}</dd>
+              </div>
+            )
+        )
       }
     </dl>
   </div>
 );
 
+const LengthChart = ({platformLength, tabularLength}) => (
+  (typeof platformLength === 'undefined' || typeof tabularLength === 'undefined')
+    ? <div />
+    : <div className='table-properties-chart'>
+      <dt>Length (ORMA)</dt>
+      <dd>
+        <div style={{
+          width: `${platformLength / (Math.max(platformLength, tabularLength)) * 100 * 0.5}%`
+        }}></div>
+        <span>{`${round(platformLength, 1)} km`}</span>
+      </dd>
+      <dt>Length (Tabular)</dt>
+      <dd>
+        <div style={{
+          width: `${tabularLength / (Math.max(platformLength, tabularLength)) * 100 * 0.5}%`
+        }}></div>
+        <span>{`${round(tabularLength, 1)} km`}</span>
+      </dd>
+    </div>
+);
 
 export default RowProperties;
