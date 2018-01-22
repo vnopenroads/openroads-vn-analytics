059fdc3e2081643bd1e372e05f3004efc9067f0b
Sajjad Anwar <sajjadkm@gmail.com>
2018-01-22 16:04:16 -0500

    remove trailing slash

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -8,7 +8,7 @@ module.exports = {
   consoleMessage: logo,
   api: 'http://api.openroads-vn.com',
   mbToken: 'pk.eyJ1Ijoib3BlbnJvYWRzIiwiYSI6InJ0aUQ2N3MifQ.R3hdFqriZr6kEUr-j_FYpg',
-  editorUrl: '//editor.openroads-vn.com/',
+  editorUrl: '//editor.openroads-vn.com',
   roadNetTileLayerUrl: 'http://api.openroads-vn.com:3000/dashboard/{z}/{x}/{y}.png',
   provinceDumpBaseUrl: 'https:/openroads-vn-dumps-prod.s3.amazonaws.com/by-province-id/'
 };
