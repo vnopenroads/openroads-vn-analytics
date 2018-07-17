68b59d89a34f9f24a0425437539c4ef2b9afb3c3
Ricardo Mestre <ricardo@duplos.org>
2018-07-17 18:12:50 +0100

    Structure and style task panel components

--- app/assets/scripts/views/playground.js
+++ app/assets/scripts/views/playground.js
@@ -20,7 +20,7 @@ var Playground = React.createClass({
 
             <figure className='map'>
 
-              <div className='map__controls map__controls--top-right map__controls--column'>
+              <div className='map__controls map__controls--column-right'>
 
                 <article className='panel task-panel'>
                   <header className='panel__header'>
@@ -37,16 +37,54 @@ var Playground = React.createClass({
                         <h1 className='task-group__title'>Select action to perform</h1>
                       </header>
                       <div className='task-group__body'>
+                        <form className='form task-group__actions'>
+                          <div className='form__group'>
+                            <label className='form__label visually-hidden'>Actions</label>
+                            <select className='form__control'>
+                              <option value='remove-duplicates'>Remove duplicates</option>
+                              <option value='create-intersection'>Create intersection</option>
+                            </select>
+                          </div>
+                        </form>
+                      </div>
+                    </section>
+
+                    <section className='task-group'>
+                      <header className='task-group__header'>
+                        <h1 className='task-group__title'>Select roads to work on</h1>
+                      </header>
+                      <div className='task-group__body'>
                         <ul className='road-list'>
                           <li className='road-list__item'>
-                            <article className='road'>
+                            <article className='road' id='road-213TX00018'>
                               <header className='road__header'>
                                 <div className='road__headline'>
                                   <h1 className='road__title'>213TX00018</h1>
                                   <p className='road__subtitle'>Thanh Hoa, Hau Loc</p>
                                 </div>
                                 <div className='road__h-actions'>
-                                  <p>Action</p>
+                                <label className='form__option form__option--custom-checkbox'>
+                                  <input type='checkbox' name='road-213TX00018--checkbox' id='road-213TX00018--checkbox' value='road-213TX00018--checkbox' />
+                                  <span className='form__option__ui'></span>
+                                  <span className='form__option__text visually-hidden'>Selected</span>
+                                </label>
+                                </div>
+                              </header>
+                            </article>
+                          </li>
+                          <li className='road-list__item'>
+                            <article className='road' id='road-213TX00019'>
+                              <header className='road__header'>
+                                <div className='road__headline'>
+                                  <h1 className='road__title'>213TX00019</h1>
+                                  <p className='road__subtitle'>Thanh Hoa, Hau Loc</p>
+                                </div>
+                                <div className='road__h-actions'>
+                                <label className='form__option form__option--custom-checkbox'>
+                                  <input type='checkbox' name='road-213TX00019--checkbox' id='road-213TX00019--checkbox' value='road-213TX00019--checkbox' />
+                                  <span className='form__option__ui'></span>
+                                  <span className='form__option__text visually-hidden'>Selected</span>
+                                </label>
                                 </div>
                               </header>
                             </article>
@@ -54,6 +92,68 @@ var Playground = React.createClass({
                         </ul>
                       </div>
                     </section>
+
+                    <section className='task-group'>
+                      <header className='task-group__header'>
+                        <h1 className='task-group__title'>Select road to intersect with</h1>
+                      </header>
+                      <div className='task-group__body'>
+                        <ul className='road-list'>
+                          <li className='road-list__item'>
+                            <article className='road' id='road-213TX00020'>
+                              <header className='road__header'>
+                                <div className='road__headline'>
+                                  <h1 className='road__title'>213TX00020</h1>
+                                  <p className='road__subtitle'>Thanh Hoa, Hau Loc</p>
+                                </div>
+                                <div className='road__h-actions'>
+                                <label className='form__option form__option--custom-radio'>
+                                  <input type='radio' name='road-group--radio' id='road-213TX00020--radio' value='road-213TX00020--radio' />
+                                  <span className='form__option__ui'></span>
+                                  <span className='form__option__text visually-hidden'>Selected</span>
+                                </label>
+                                </div>
+                              </header>
+                            </article>
+                          </li>
+                          <li className='road-list__item'>
+                            <article className='road' id='road-213TX00020'>
+                              <header className='road__header'>
+                                <div className='road__headline'>
+                                  <h1 className='road__title'>213TX00021</h1>
+                                  <p className='road__subtitle'>Thanh Hoa, Hau Loc</p>
+                                </div>
+                                <div className='road__h-actions'>
+                                <label className='form__option form__option--custom-radio'>
+                                  <input type='radio' name='road-group--radio' id='road-213TX00021--radio' value='road-213TX00021--radio' />
+                                  <span className='form__option__ui'></span>
+                                  <span className='form__option__text visually-hidden'>Selected</span>
+                                </label>
+                                </div>
+                              </header>
+                            </article>
+                          </li>
+                        </ul>
+                      </div>
+                    </section>
+
+                    <section className='task-group'>
+                      <header className='task-group__header'>
+                        <h1 className='task-group__title'>Select VPROMMID to Apply</h1>
+                      </header>
+                      <div className='task-group__body'>
+                        <form className='form task-group__actions'>
+                          <div className='form__group'>
+                            <label className='form__label visually-hidden'>Actions</label>
+                            <select className='form__control'>
+                              <option value='vprommid-213TX00021'>213TX00021</option>
+                              <option value='vprommid-213TX00022'>213TX00022</option>
+                            </select>
+                          </div>
+                        </form>
+                      </div>
+                    </section>
+
                   </div>
                   <footer className='panel__footer'>
                     <div className='panel__f-actions'>
