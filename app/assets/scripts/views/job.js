d48e818b94e3a41f6f005bf392a02e6ae7c24b26
Sanjay Bhangar <sanjaybhangar@gmail.com>
2018-08-27 13:21:10 +0530

    poll backend for updates if job is not finished + show simple text for status and message

--- app/assets/scripts/views/job.js
+++ app/assets/scripts/views/job.js
@@ -8,6 +8,11 @@ import {
   fetchJob
 } from '../redux/modules/jobs';
 import { connect } from 'react-redux';
+import T, {
+  translate
+} from '../components/t';
+
+const STATUS_POLL_INTERVAL = 2000; // in ms
 
 var Job = React.createClass({
   // propTypes: {
@@ -25,18 +30,34 @@ var Job = React.createClass({
         job: nextProps.job,
       });
     }
+
+    // if job does not have a return value yet, keep polling backend for status
+    if (!nextProps.job.returnValue) {
+      setTimeout(() => {
+        this.props.fetchJob(this.props.params.id);
+      }, STATUS_POLL_INTERVAL);
+    }
   },
   render: function () {
-    const { params } = this.props;
+    const { params, language } = this.props;
     const { job } = this.state;
     const id = params.id;
+    let status;
+    let msg = '';
+    if (job && job.returnvalue) {
+      status = translate(language, 'Finished');
+      msg = job.returnvalue;
+    } else {
+      status = translate(language, 'Processing upload...');
+    }
     return (
       <div className='inpage__body'>
         <div className='inner'>
-          <h2>Job page - { id }</h2>
-          <div>
-            <pre>{ job && JSON.stringify(job) }</pre>
-          </div>
+          <h2>Job page - #{ id }</h2>
+          <p>
+            Status: { status }
+          </p>
+          { msg && <p> { msg } </p> }
         </div>
       </div>
     );
