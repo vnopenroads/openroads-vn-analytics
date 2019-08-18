ba47da1001ef390ab1a7ad89507f706d000f0c0a
Nguyen Nghia <ngd.nghia28@gmail.com>
2019-08-18 21:10:34 +0700

    change email of contract us

--- app/assets/scripts/views/faq.js
+++ app/assets/scripts/views/faq.js
@@ -145,7 +145,7 @@ const Faq = ({ language }) => (
         <section>
           <p>
             <T>If you have any remaining questions</T>
-            <a href={`mailto:dphan2@worldbank.org?cc=gost@worldbank.org&subject=${translate(language, 'Question regarding ORMA, not answered in the FAQ')}`}>
+            <a href={`mailto:phan2@worldbank.org?cc=ptran1@worldbank.org&cc=cknguyen@worldbank.org&cc=tranthikimdang@gmail.com&cc=tailm.fu@gmail.com&subject=${translate(language, 'Question regarding ORMA, not answered in the FAQ')}`}>
               <button className="button button--base-raised-light"><T>email us</T></button>
             </a>
           </p>
