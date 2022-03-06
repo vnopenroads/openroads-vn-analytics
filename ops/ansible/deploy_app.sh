a57f4ef49fe62bdb788ef0ee2e7ef68d4ffc05ac
Jamie Cook <jamie.cook@veitchlister.com.au>
2022-03-06 16:57:38 +1000

    deployment stuff

--- ops/ansible/deploy_app.sh
+++ ops/ansible/deploy_app.sh
@@ -3,9 +3,23 @@
 set -eu
 
 main() {
+    [[ -f .env ]] && export $(cat .env | xargs)
+    local env=$1
+    [[ ${env} == "production" ]] && deploy_prod
+    [[ ${env} == "uat" ]] && deploy_uat
+}
+
+deploy_uat() {
     for i in node-app cba-api react-app; do
-        ./1.provision.sh --tags=${i}
+        echo ./1.provision.sh --tags=${i}
     done
+}
+
+deploy_prod() {
+    local ansible_cmd="ansible-playbook -i inventory playbook.yaml --extra-vars \"ansible_sudo_pass=${CBA_PASSWORD}\""
+    ${ansible_cmd} --tags=cba-api -l orma-api
+    # ${ansible_cmd} --tags=node-app
+    # ${ansible_cmd} --tags=react-app
 }
 
 main "$@"
