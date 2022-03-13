39aeceb1fe463ea376c7758cb982f724d8767dd7
Jamie Cook <jamie.cook@veitchlister.com.au>
2022-03-13 12:03:54 +1000

    add temporary mapbox token

--- app/assets/scripts/config/uat.js
+++ app/assets/scripts/config/uat.js
@@ -5,6 +5,7 @@
 module.exports = {
   environment: 'uat',
   api: 'http://orma-uat.drvn.gov.vn:4000',
-  mbToken: 'pk.eyJ1IjoidGFpbG0xIiwiYSI6ImNqeXJobTVxZTBiOG8zaG82Mm4xbHBocHAifQ.Rr66mzvqmKde3GDspwbXhw',
+  // mbToken: 'pk.eyJ1IjoidGFpbG0xIiwiYSI6ImNqeXJobTVxZTBiOG8zaG82Mm4xbHBocHAifQ.Rr66mzvqmKde3GDspwbXhw',
+  mbToken: 'pk.eyJ1IjoiamFtaWVjb29rIiwiYSI6ImNsMG9tcXMwZzFzMjgzbG9hbXEzczQybmsifQ.eZVt5dA4woL2e2OD7uApTw',
   roadNetTileLayerUrl: 'http://orma.drvn.gov.vn/tilemap/{z}/{x}/{y}.png',
 };
